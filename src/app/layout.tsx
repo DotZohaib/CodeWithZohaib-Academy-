5c62a6bbb0ccdd18982a6d6b6342df21680862f5
ZOHAIB ALI DAYO <154052553+DotZohaib@users.noreply.github.com>
2024-11-08 00:30:14 +0500

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-// src/app/layout.tsx
-
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/theme-provider";
+import Providers from "@/components/ProgressBarProvider";
 
 // Font Setup
 const geistSans = localFont({
@@ -25,14 +24,12 @@ export const metadata: Metadata = {
   description: "CodeWithZohaib offers an extensive range of courses to empower you with in-depth knowledge in Next.js, JavaScript, Python, C++, and many other programming languages and frameworks. Whether you're a beginner or an advanced developer, our expert-led tutorials, hands-on projects, and interactive lessons are designed to help you master coding skills and excel in the tech world. Join us today for access to up-to-date resources and start your journey to becoming a skilled developer across all major languages and technologies.",
 };
 
-
-
 // RootLayout Component
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
@@ -44,9 +41,11 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
+          <Providers>
+            <Navbar />
+            {children}
+            <Footer />
+          </Providers>
         </ThemeProvider>
       </body>
     </html>
