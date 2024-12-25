9334bd4fc433838e74d5881e0167ac52ce552061
ZOHAIB ALI DAYO <154052553+DotZohaib@users.noreply.github.com>
2024-12-25 09:47:30 +0500

    page.tsx

--- src/app/Next/page.tsx
+++ src/app/Next/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
+import Prism from "prismjs";
 import { Search, Filter, BookOpen } from 'lucide-react';
 import {
   Card,
@@ -850,6 +851,10 @@ const Next = () => {
   const handleLoadMore = () => {
     setShowCount(prev => prev + 10);
   };
+
+    useEffect(() => { 
+    Prism.highlightAll();
+  }, []); 
 
   return (
     <div className="max-w-4xl mx-auto p-4 space-y-6">
